79a69d12c3f60e7c7353eae66effa8494350ba03
zhaojingru <327256087.qq.com>
2019-02-27 20:12:38 +0800

    菜单路由跳转

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import {Menu,Icon} from 'antd';
 
+import {NavLink} from 'react-router-dom';
+
 
 //引入文件+图片
 import logo from '../../assets/images/logo.png';
@@ -15,26 +17,35 @@ export default class LeftNav extends Component{
   render(){
     return(
       <div className="left-nav">
-        <header className="left-header">
-          <img src={logo} alt="logo"/>
-          <h2>硅谷后台</h2>
+        <header>
+          <NavLink to='/home' className="left-header">
+            <img src={logo} alt="logo"/>
+            <h2>硅谷后台</h2>
+          </NavLink>
         </header>
 
         <Menu theme="dark" mode="inline">
           {/*一级菜单*/}
           <Item>
-            <Icon type="home" />
-            <span>首页</span>
+            <NavLink to='/home'>
+              <Icon type="home" />
+              <span>首页</span>
+            </NavLink>
           </Item>
+
           {/*二级菜单*/}
           <SubMenu title={<span><Icon type="appstore" /><span>商品</span></span>}>
             <Item>
-              <Icon type="bars" />
-              <span>品类管理</span>
+              <NavLink to='/category'>
+                <Icon type="bars" />
+                <span>品类管理</span>
+              </NavLink>
             </Item>
             <Item>
-              <Icon type="tool" />
-              <span>商品管理</span>
+              <NavLink to='/product'>
+                <Icon type="tool" />
+                <span>商品管理</span>
+              </NavLink>
             </Item>
           </SubMenu>
         </Menu>
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Row,Col} from 'antd';
+import {Route,Switch,Redirect} from 'react-router-dom'
 
 
 //引入文件
@@ -7,6 +8,9 @@ import './index.less';
 import LeftNav from '../../components/left-nav';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import Home from '../home';
+import Category from '../category';
+import Product from '../product';
 
 
 export default class Admin extends Component {
@@ -18,7 +22,14 @@ export default class Admin extends Component {
         </Col>
         <Col span={20}>
           <Header />
-          <div className="admin-content">content</div>
+          <div className="admin-content">
+            <Switch>
+              <Route path='/home' component={Home} />
+              <Route path='/category' component={Category} />
+              <Route path='/product' component={Product} />
+              <Redirect to='/home'/>
+            </Switch>
+          </div>
           <Footer />
         </Col>
       </Row>
